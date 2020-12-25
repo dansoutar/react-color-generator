0311f2ce50cf1017f9873ae1cc79ae577a17d823
Dan Soutar <dan.soutar@littlerocket.io>
2020-12-25 12:14:09 -0500

    finished color generator project

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
 
-export default SingleColor
+  const hex = rgbToHex(...rgb);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+      return () => clearTimeout(timeout);
+    }, 3000);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 7 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hex}%</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
